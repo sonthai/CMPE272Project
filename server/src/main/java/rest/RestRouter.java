1f7b8774aeefbf30adb1d08757f33a08925a2d85
Son Thai <thaison242@gmail.com>
2016-10-23 01:08:33 -0700

    Fix localhost

--- server/src/main/java/rest/RestRouter.java
+++ server/src/main/java/rest/RestRouter.java
@@ -7,7 +7,7 @@ import services.HelloWorldService;
 public class RestRouter extends RouteBuilder {
     public void configure() throws Exception {
         restConfiguration().component("restlet")
-                .host("localhost")
+                .host("127.0.0.1")
                 .port(9090)
                 .bindingMode(RestBindingMode.auto);
 
